38ef762aebd6e454ac1adec40b48527761bd3f9a
আলি হাসান <101947194+dev-alihasan@users.noreply.github.com>
2023-02-21 00:16:42 +0600

    Create script0.js

---
+++ script0.js
@@ -0,0 +1,12 @@
+//Here's one solution in JavaScript:
+function twoSum(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
